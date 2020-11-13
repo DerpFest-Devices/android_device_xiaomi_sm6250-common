0d6fb76b9d707bffc1c8138e69670e4ed4ea842c
Volodymyr Zhdanov <wight554@gmail.com>
2020-11-13 21:59:52 +0530

    sm6250-common: build qti health@2.1 hal

--- common.mk
+++ common.mk
@@ -101,6 +101,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.qcom
 
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl-qti \
+    android.hardware.health@2.1-service
+
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
