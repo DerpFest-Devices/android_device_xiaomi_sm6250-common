93163cf046cd97045e2242816f5f05a29ead0b85
PIPIPIG233666 <pig.priv@gmail.com>
2020-11-14 07:30:36 +0530

    sm6250-common: Split component-overrides to support pure aosp/qti bt stack

--- common.mk
+++ common.mk
@@ -117,7 +117,8 @@ PRODUCT_PACKAGES += \
 
 # Component override
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
+    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml \
+    $(LOCAL_PATH)/configs/component-overrides_qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/component-overrides.xml
 
 # Context hub HAL
 PRODUCT_PACKAGES += \
