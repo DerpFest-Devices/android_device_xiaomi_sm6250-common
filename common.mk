21ebbfa5ea56a483d0654296ebb87e1bf89ad36b
Volodymyr Zhdanov <wight554@gmail.com>
2020-11-14 07:15:59 +0530

    sm6250-common: Remove common consumer IR HAL
    * it was never needed

--- common.mk
+++ common.mk
@@ -186,8 +186,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:system/usr/keylayout/uinput-goodix.kl
 
 # IR
-PRODUCT_PACKAGES += \
-    android.hardware.ir@1.0-service.xiaomi
 
 # Light
 PRODUCT_PACKAGES += \
