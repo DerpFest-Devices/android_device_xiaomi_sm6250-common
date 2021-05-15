b617f5ed4bc69f1f35404f14d59fdf2a28aa3cb0
klozz <klozz@TheXPerienceProject.org>
2021-05-15 23:11:21 +0200

    sm6250-common: Remove custom OTA leftover
    Signed-off-by: klozz <klozz@TheXPerienceProject.org>

--- common.mk
+++ common.mk
@@ -16,9 +16,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
-# OTA overlay
-$(call inherit-product, device/xiaomi/sm6250-common/updater_ota.mk)
-
 # AID/fs configs
 PRODUCT_PACKAGES += \
     fs_config_files
